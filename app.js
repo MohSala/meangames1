4b09d7aa26e8068d5f3b85e30fe8552b626c31a4
Mohammed Sanusi <muhammeds90@yahoo.com>
2021-09-28 17:12:45 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set("port", 5353);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const server = app.listen(app.get("port"), () => {
+    console.log(`now listening on ${server.address().port}`);
+});
